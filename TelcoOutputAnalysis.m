835b6fef2fdb734e0d0286ee13dedfe6aca44e3e
Hendrik Simon Baacke <hendrikbaacke@gmail.com>
2020-05-23 13:16:57 +0200

    Validation of Simulation
    1. for the service times of consumers we have a bug: In some cases we have a service time of 0, this is not dependent on the type of handling agent nor on the time within the simulation
    2. the thinning algorithm gives exponentially distributed interarrival times, but we generate arrivals of customers at a too high rate lambda
    both problems occur for consumer costumers, for corporate customers it looks okay

--- TelcoOutputAnalysis.m
+++ TelcoOutputAnalysis.m
@@ -2,6 +2,7 @@
 k = 1;
 
 C_data = cell(k,1);
+SortedC_data = cell(k,1);
 WaitCons_data = cell(k,1);
 WaitCorp_data = cell(k,1);
 for i = 1:k
@@ -9,18 +10,22 @@ i = i-1;
 %retrieve the call data
 %CHANGE THE FILEPATH TO THE DESIRED FILEPATH OF ALL RELEVANT CSV FILES
 fileN = ['/Users/HendrikS/Documents/GitHub/Telco_Simulation/informationCalls',num2str(i),'.csv'];
-C_data{i+1} = readtable(fileN)
+C_data{i+1} = readtable(fileN);
 C_data{i+1}.Properties.VariableNames = {'cstm_tp' 'CSA_tp' 'tme_incoming' 'tme_start' 'tme_end'};
+
+% sort C_data according to tme_incoming
+SortedC_data{i+1} = sortrows(C_data{i+1},3);
 
 %retrieve the waiting times of consumer costumers
 %CHANGE THE FILEPATH TO THE DESIRED FILEPATH OF ALL RELEVANT CSV FILES
 fileWaitCons = ['/Users/HendrikS/Documents/GitHub/Telco_Simulation/waitingTimesConsumer',num2str(i),'.csv'];
-WaitCons_data{i+1} = readtable(fileWaitCons)
+WaitCons_data{i+1} = readtable(fileWaitCons);
 WaitCons_data{i+1}.Properties.VariableNames = {'consumer_wait_tme'};
 
 %retrieve the waiting times of corporate costumers
 %CHANGE THE FILEPATH TO THE DESIRED FILEPATH OF ALL RELEVANT CSV FILES
 fileWaitCorp = ['/Users/HendrikS/Documents/GitHub/Telco_Simulation/waitingTimesCorporate',num2str(i),'.csv'];
+WaitCorp_data{i+1} = readtable(fileWaitCorp);
 WaitCorp_data{i+1}.Properties.VariableNames = {'corporate_wait_tme'};
 end
 
@@ -37,19 +42,24 @@ serviceTmeCorp = cell(repVal,1);
 
 for i = 1:repVal
 %get all data from consumer calls from each sim. run
-ConsTbl = (C_data{i}.cstm_tp ==0);
-consData{i} = C_data{i}(ConsTbl,:);
+ConsTbl = (SortedC_data{i}.cstm_tp ==0);
+consData{i} = SortedC_data{i}(ConsTbl,:);
+
+%get all data from corporate calls from each sim. run
+CorpTbl = (SortedC_data{i}.cstm_tp ==1);
+corpData{i} = SortedC_data{i}(CorpTbl,:);
+
 %get the service times of consumers
 serviceTmeCons{i} = consData{i}{:,5}-consData{i}{:,4};
 d1 = ['Shortest consumer call service time in ',num2str(i), '. run: ']; 
 d2 = ['Longest consumer call service time in ',num2str(i), '. run: ' ];
 disp(d1)
+%see next section                                                    !!
 disp(min(serviceTmeCons{i}))
 disp(d2)
+%highest values are not truncated
 disp(max(serviceTmeCons{i}))
-%get all data from corporate calls from each sim. run
-CorpTbl = (C_data{i}.cstm_tp ==1);
-corpData{i} = C_data{i}(CorpTbl,:);
+
 %get the service times of corporates
 serviceTmeCorp{i} = corpData{i}{:,5}-corpData{i}{:,4};
 d3 = ['Shortest corporate call service time in ',num2str(i), '. run: ']; 
@@ -60,29 +70,69 @@ disp(min(serviceTmeCorp{i}))
 disp(d4)
 %highest values are not truncated
 disp(max(serviceTmeCorp{i}))
+
+%get the interarrival tmes of consumers
+%Simulation starts at 21600
+
+interTmeCons{i}(1) =  (consData{i}{1:1,{'tme_incoming'}}-21600);
+for j = 1:(height(consData{i})-1)  
+interTmeCons{i}(j+1) = (consData{i}{j+1:j+1,{'tme_incoming'}}-consData{i}{j:j,{'tme_incoming'}});
 end
 
+%get the interarrival tmes of corporates
+
+interTmeCorp{i}(1) =  (corpData{i}{1:1,{'tme_incoming'}}-21600);
+
+for j = 1:(height(corpData{i})-1)  
+interTmeCorp{i}(j+1) = (corpData{i}{j+1:j+1,{'tme_incoming'}}-corpData{i}{j:j,{'tme_incoming'}});
+end
+
+end
+
+%%
 %print the data
 %consData{1}
 %corpData{1}
-
+%______________
+%embedd this in loop of previous section if wish to plot more runs
+%here only the data from the first run is used
+%plot a distribution histogram, superimposing normal dist. for
+%consumer and corporate service times
 figure(1)
 histfit(serviceTmeCons{1})
-%plot a distribution histogram, superimposing normal for
-%corporate service times
+ylabel('amount of occurrence');
+xlabel('service time in sec');
+title('Consumer data: Fitting normal to observed service tmes');
 figure(2)
 histfit(serviceTmeCorp{1})
+ylabel('amount of occurrence');
+xlabel('service time in sec');
+title('Corporate data: Fitting normal to observed service tmes');
+
+%plot a distribution histogram, superimposing exponential dist. for
+%consumer and corporate interarrival times
+figure(3)
+histfit(interTmeCons{1},[],'exponential')
+ylabel('amount of occurrence');
+xlabel('interarrival time in sec');
+title('Consumer data: Fitting exponential to observed interarrvial tmes(s)');
+figure(4)
+histfit(interTmeCorp{1},[],'exponential')
+ylabel('amount of occurrence');
+xlabel('interarrival time in sec');
+title('Corporate data: Fitting exponential to observed interarrvial tmes(s)');
 
 %% 
-% finding bug
+% finding bug in service tme of consumers
 irr_stmCons= (serviceTmeCons{1}<25)
 
 S_cons = sort(serviceTmeCons{1});
 
-S_cons(1:505)
+irregularEntries = ((consData{1}.tme_end - consData{1}.tme_start)<25);
+
+S_cons(1:height(consData{1}(irregularEntries,:))+1)
 length(serviceTmeCons{1}(irr_stmCons))
 
-irregularEntries = ((consData{1}.tme_end - consData{1}.tme_start)<25);
 
 consData{1}(irregularEntries,:)
 
@@ -90,30 +140,16 @@ consData{1}(irregularEntries,:)
 %consData{1}((consData{1}{:,5}-consData{1}{:,4})<25)
 
 
-
 %corpData{1}(1:5,{'tme_start','tme_end'})
-
-
-
-
 
 %largest value can in theory be arbitrarily large:
 
+%%
+ 
 
-
-
-
-
-
-
-
-
-%22/05/20
-
-
-
-
-
+% interTmeCorp{1}(1:15)
+% corpData{1}(1:15,{'cstm_tp' 'CSA_tp' 'tme_incoming' 'tme_start' 'tme_end'})
+% disp((corpData{1}{1+1:1+1,{'tme_incoming'}}-corpData{1}{1:1,{'tme_incoming'}}))
 
 
 
